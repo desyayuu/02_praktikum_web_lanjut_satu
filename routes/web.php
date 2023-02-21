a6d8d914acd02fa332133678fb8a1c68e652cff7
Desy Ayurianti <desii.ayurianti@gmail.com>
2023-02-21 23:25:32 +0700

    first add praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Nama: Desy Ayurianti<br>";
+    echo "NIM: 21417201119";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID {$id}";
+});
